e0cc351cd2e64575d1124abed2f23ed6941bfe91
Dadv-a11y <darilvia64@gmail.com>
2025-05-05 13:20:51 +0100

    delete

--- src/main/java/com/example/librarymanager/Database/BaseDeDonnee.java
+++
@@ -1,7 +0,0 @@
-package com.example.librarymanager.Database;
-
-public interface BaseDeDonnee<T> {
-    public void create(T entity) ;
-    public void Update(T entity) ;
-    public void Delete(int id);
-}
