7493db95f97bcf38b5b3eca48aa8063716028941
Djifack daril <darilvia64@gmail.com>
2025-05-08 06:58:03 +0100

    connexion des pages login et registration a la BD

--- src/main/java/com/example/librarymanager/Controllers/RegistrationController.java
+++ src/main/java/com/example/librarymanager/Controllers/RegistrationController.java
@@ -1,9 +1,15 @@
 package com.example.librarymanager.Controllers;
+import java.sql.SQLException;
+
+import com.example.librarymanager.Database.UserTable;
+import com.example.librarymanager.Models.User;
 
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+
+import com.example.librarymanager.utils.Alertmessage;
 
 public class RegistrationController {
     public Button RegisterButton;
@@ -13,4 +19,42 @@ public class RegistrationController {
     public PasswordField RegistrationPassword;
     public TextField RegistrationIdentifier;
     public TextField Username;
+   
+    public void RegisterButtonOnAction() {
+        String username = Username.getText().trim();
+        String password = RegistrationPassword.getText().trim();
+        String confirmedPassword = ConfirmedPassword.getText().trim();
+        String email = RegistrationIdentifier.getText().trim();
+
+        if (username.isEmpty() || password.isEmpty() || confirmedPassword.isEmpty() || email.isEmpty()) {
+          Alertmessage.showAlert("ERROR","All fields are required.");
+            return;
+        }
+
+        if (!password.equals(confirmedPassword)) {
+            Alertmessage.showAlert("ERROR","The passwords are not the same");
+            return;
+        }
+      try {
+        User user = new User();
+        user.setFullName(username);
+        user.setPassword(confirmedPassword);
+        user.setEmail(email);
+        UserTable userTable = new UserTable() ;
+        userTable.create(user);
+      }catch(SQLException e){
+        System.err.println("ERROR:"+e);
+        Alertmessage.showAlert("ERROR","the Registration fail due to an internal problem");
+        return ;
+      }
+       catch (Exception e) {
+        System.err.println("ERROR:"+e);
+        Alertmessage.showAlert("ERROR","the Registration fail due to an internal problem");
+        return ;
+      }
+      LogInRedirectOnAction();
+    }
+    public void LogInRedirectOnAction() {
+        // Redirect to login page
+    }
 }
